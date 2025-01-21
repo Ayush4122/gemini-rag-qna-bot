48fa3ade25a56b82d46a1673a52bfa738a69a9e5
Ayush Sharma <sharma.ayush4122@gmail.com>
2025-01-22 00:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 import os
 from dotenv import load_dotenv
